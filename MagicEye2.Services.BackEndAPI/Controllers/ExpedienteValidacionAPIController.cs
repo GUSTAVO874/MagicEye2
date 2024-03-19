8bfd5b9a122471df6f460b303d7b288c19bb137e
GUSTAVO874 <gustavo8156@gmail.com>
2024-03-18 22:53:31 -0500

    commit22, post expediente, validación y cobertura todo ok

--- MagicEye2.Services.BackEndAPI/Controllers/ExpedienteValidacionAPIController.cs
+++ MagicEye2.Services.BackEndAPI/Controllers/ExpedienteValidacionAPIController.cs
@@ -25,13 +25,57 @@ namespace MagicEye2.Services.BackEndAPI.Controllers
                 return BadRequest();
             }
 
+            //using (var transaction = _db.Database.BeginTransaction())
+            //{
+            //    try
+            //    {
+            //        var expediente = new Expediente
+            //        {
+            //            // Set properties from DTO
+            //            DtosImprescindiblesOK = true,
+            //        };
+
+            //        _db.Expedientes.Add(expediente);
+            //        await _db.SaveChangesAsync();
+
+            //        var validacion = new Validacion
+            //        {
+            //            ExpedienteId = expediente.ExpedienteId,
+            //            // Set other properties from DTO
+            //            NombreDto = "asdf",
+            //        };
+
+            //        _db.Validacions.Add(validacion);
+
+            //        var cobertura = new Cobertura
+            //        {
+            //            ExpedienteId = expediente.ExpedienteId,
+            //            // Set other properties from DTO
+            //            RecognitionProcessOK = true,
+            //        };
+
+            //        _db.Coberturas.Add(cobertura);
+
+            //        await _db.SaveChangesAsync();
+            //        await transaction.CommitAsync();
+
+            //        return Ok(new { expediente.ExpedienteId });
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        await transaction.RollbackAsync();
+            //        // Log the exception (ex)
+            //        return StatusCode(500, "Internal server error");
+            //    }
+            //}
             using (var transaction = _db.Database.BeginTransaction())
             {
                 try
                 {
                     var expediente = new Expediente
                     {
-                        // Set properties from DTO
+                        // Inicializa las propiedades del Expediente desde el DTO
+                        DtosImprescindiblesOK = true,
                     };
 
                     _db.Expedientes.Add(expediente);
@@ -40,7 +84,8 @@ namespace MagicEye2.Services.BackEndAPI.Controllers
                     var validacion = new Validacion
                     {
                         ExpedienteId = expediente.ExpedienteId,
-                        // Set other properties from DTO
+                        // Inicializa las demás propiedades de Validacion desde el DTO
+                        NombreDto = "asdf",
                     };
 
                     _db.Validacions.Add(validacion);
@@ -48,12 +93,22 @@ namespace MagicEye2.Services.BackEndAPI.Controllers
                     var cobertura = new Cobertura
                     {
                         ExpedienteId = expediente.ExpedienteId,
-                        // Set other properties from DTO
+                        // Inicializa las demás propiedades de Cobertura desde el DTO
+                        RecognitionProcessOK = true,
                     };
 
                     _db.Coberturas.Add(cobertura);
 
+                    // Primero guardamos las entidades Validacion y Cobertura para que se generen los IDs.
                     await _db.SaveChangesAsync();
+
+                    // Ahora, actualizamos el Expediente con los IDs generados.
+                    expediente.ValidacionId = validacion.ValidacionId;
+                    expediente.CoberturaId = cobertura.CoberturaId;
+
+                    // Guardamos de nuevo para actualizar el Expediente con los IDs de Validacion y Cobertura.
+                    await _db.SaveChangesAsync();
+
                     await transaction.CommitAsync();
 
                     return Ok(new { expediente.ExpedienteId });
@@ -61,10 +116,11 @@ namespace MagicEye2.Services.BackEndAPI.Controllers
                 catch (Exception ex)
                 {
                     await transaction.RollbackAsync();
-                    // Log the exception (ex)
+                    // Aquí se debería hacer un log del error.
                     return StatusCode(500, "Internal server error");
                 }
             }
+
         }
     }
 }
---
+++ MagicEye2.Services.BackEndAPI/Migrations/20240319032016_migracion14.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicEye2.Services.BackEndAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class migracion14 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cobertura_Expedientes_ExpedienteId",
+                table: "Cobertura");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Cobertura",
+                table: "Cobertura");
+
+            migrationBuilder.RenameTable(
+                name: "Cobertura",
+                newName: "Coberturas");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Cobertura_ExpedienteId",
+                table: "Coberturas",
+                newName: "IX_Coberturas_ExpedienteId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Coberturas",
+                table: "Coberturas",
+                column: "CoberturaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Coberturas_Expedientes_ExpedienteId",
+                table: "Coberturas",
+                column: "ExpedienteId",
+                principalTable: "Expedientes",
+                principalColumn: "ExpedienteId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Coberturas_Expedientes_ExpedienteId",
+                table: "Coberturas");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Coberturas",
+                table: "Coberturas");
+
+            migrationBuilder.RenameTable(
+                name: "Coberturas",
+                newName: "Cobertura");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Coberturas_ExpedienteId",
+                table: "Cobertura",
+                newName: "IX_Cobertura_ExpedienteId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Cobertura",
+                table: "Cobertura",
+                column: "CoberturaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cobertura_Expedientes_ExpedienteId",
+                table: "Cobertura",
+                column: "ExpedienteId",
+                principalTable: "Expedientes",
+                principalColumn: "ExpedienteId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
